9af3fefbe908fabe3409b623e95fb6ac93607f62
Harshithachalapathi <harshithac24052004@gmail.com>
2026-03-13 20:48:51 +0530

    Update alu_design.v

--- alu_design.v
+++ alu_design.v
@@ -1,25 +1,38 @@
-module alu_8bit(
-input [7:0] A,
-input [7:0] B,
-input [2:0] sel,
-output reg [7:0] result
+`timescale 1ns/1ps
+
+module alu_tb;
+
+reg [7:0] A;
+reg [7:0] B;
+reg [2:0] sel;
+wire [7:0] result;
+
+alu_8bit uut(
+.A(A),
+.B(B),
+.sel(sel),
+.result(result)
 );
 
-always @(*) begin
-case(sel)
+initial begin
 
-3'b000: result = A + B;
-3'b001: result = A - B;
-3'b010: result = A & B;
-3'b011: result = A | B;
-3'b100: result = A ^ B;
-3'b101: result = ~A;
-3'b110: result = A << 1;
-3'b111: result = A >> 1;
+$dumpfile("dump.vcd");
+$dumpvars(0, alu_tb);
 
-default: result = 8'b00000000;
+A = 8'b00001111;
+B = 8'b00000011;
 
-endcase
+sel = 3'b000; #10;
+sel = 3'b001; #10;
+sel = 3'b010; #10;
+sel = 3'b011; #10;
+sel = 3'b100; #10;
+sel = 3'b101; #10;
+sel = 3'b110; #10;
+sel = 3'b111; #10;
+
+$finish;
+
 end
 
 endmodule
