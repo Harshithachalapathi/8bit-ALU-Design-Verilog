804e51a164ba0bc23ab8105ffa1aaac919de567f
Harshithachalapathi <harshithac24052004@gmail.com>
2026-03-13 20:27:35 +0530

    Create alu_testbench.v

---
+++ alu_testbench.v
@@ -0,0 +1,32 @@
+`timescale 1ns/1ps
+
+module alu_tb;
+
+reg [7:0] A;
+reg [7:0] B;
+reg [2:0] sel;
+wire [7:0] result;
+
+alu_8bit uut(
+.A(A),
+.B(B),
+.sel(sel),
+.result(result)
+);
+
+initial begin
+
+A = 8'b00001111;
+B = 8'b00000011;
+
+sel = 3'b000; #10;
+sel = 3'b001; #10;
+sel = 3'b010; #10;
+sel = 3'b011; #10;
+sel = 3'b100; #10;
+
+$finish;
+
+end
+
+endmodule
